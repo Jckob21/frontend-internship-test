481d4edbcac37150bcb6a2b4086297deca66f764
Jacob <jckob21@gmail.com>
2021-03-22 11:28:51 +0100

    Stage 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 /* Here goes your JS code */
+const popupBtn = document.getElementById("show-popup-form");
+const popupForm = document.getElementsByClassName("popup")[0];
+const closeBtn = document.getElementById("closebtn");
+
+
+popupBtn.addEventListener("click", showLoginForm);
+closeBtn.addEventListener("click", closeLoginForm);
+
+
+function showLoginForm()
+{
+    popupBtn.style.display = "none";
+    popupForm.style.display = "flex";
+}
+
+function closeLoginForm()
+{
+    popupBtn.style.display = "inline-block";
+    popupForm.style.display = "none";
+}
